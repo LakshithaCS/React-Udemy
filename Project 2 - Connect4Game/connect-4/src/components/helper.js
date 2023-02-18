3057383c435a321e48c36a72c0c55efff4ec2d12
LakshithaCS <e17338@eng.pdn.ac.lk>
2023-02-19 00:39:40 +0530

    project02 - Added AI

--- Project 2 - Connect4Game/connect-4/src/components/helper.js
+++ Project 2 - Connect4Game/connect-4/src/components/helper.js
@@ -40,7 +40,7 @@ export const isDraw = (gameBoard, currpos, currentPlayer) => {
 
 }
 
-export const guessMove = (gameBoard) => {
+export const guessMoveRandom = (gameBoard) => {
 
     const notClicked = []
     for (let i = 0; i < gameBoard.length; i++) {
@@ -51,4 +51,65 @@ export const guessMove = (gameBoard) => {
 
     const random = Math.floor(Math.random() * notClicked.length);
     return notClicked[random];
+}
+
+const getPosition = (gameBoard, moveChecks) => {
+    for (let check = 0; check < moveChecks.length; check++) {
+        for (let i = 0; i < moveChecks[check].max; i += moveChecks[check].step) {
+            let series = gameBoard[i + moveChecks[check].index[0]].toString() +
+                gameBoard[i + moveChecks[check].index[1]].toString() +
+                gameBoard[i + moveChecks[check].index[2]].toString() +
+                gameBoard[i + moveChecks[check].index[3]].toString();
+
+            switch (series) {
+                case "1110":
+                case "2220":
+                    return i + moveChecks[check].index[3];
+                case "1101":
+                case "2202":
+                    return i + moveChecks[check].index[2];
+                case "1011":
+                case "2022":
+                    return i + moveChecks[check].index[1];
+                case "0111":
+                case "0222":
+                    return i + moveChecks[check].index[0];
+            }
+        }
+    }
+
+    return -1;
+}
+
+export const guessMove = (gameBoard) => {
+
+    let moveClicks = [
+        {
+            index: [0, 4, 8, 12],
+            max: 4,
+            step: 1
+        },
+        {
+            index: [0, 1, 2, 3],
+            max: 16,
+            step: 4
+        },
+        {
+            index: [0, 5, 10, 15],
+            max: 16,
+            step: 16
+        },
+        {
+            index: [3, 6, 9, 12],
+            max: 16,
+            step: 16
+        }
+    ]
+
+    let pos = getPosition(gameBoard, moveClicks);
+    if (pos !== -1) {
+        return pos;
+    }
+    return guessMoveRandom(gameBoard);
+
 }
